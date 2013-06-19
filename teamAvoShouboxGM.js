77d301352516a71c437e722a1e1d3d1cada18708
rakiru <rakiru@fuckingwin.com>
2013-06-19 19:25:02 +0100

    Created GreaseMonkey version (simply injects minified version into page)

---
+++ teamAvoShouboxGM.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name          Team Avolition Shoutbox Enhancements
+// @description   Various enhancements for the Team Avolition shoutbox
+// @include       /^http://www.teamavolition.com/index.php\?app=forums\/?$/
+// @include       /^http://www.teamavolition.com/shoutbox\/?$/
+// @version       1.0
+// @updateURL     https://raw.github.com/rakiru/team_avo_shoutbox_enhancements/master/teamAvoShoutboxGM.js
+// @icon          http://www.teamavolition.com/favicon.ico
+// ==/UserScript==
+
+var script = document.createElement("script");
+script.setAttribute("src", "https://raw.github.com/chall8908/team_avo_shoutbox_enhancements/master/teamAvoShoutbox.min.js");
+document.body.appendChild(script);
